c5798ac2683fdae2b5f0d22f198738485b22d662
Fabian Chan <fabeschan@gmail.com>
2014-07-09 15:15:57 +0800

    Put bot arg-parser and runner into IrcDaemon class

--- ircBotBase.py
+++ ircBotBase.py
@@ -138,20 +138,24 @@ class IrcDaemon(Daemon):
         reactor.connectTCP("irc.snoonet.org", 6667, self.f) # connect factory to this host and port
         reactor.run() # run bot
 
+    def main(self):
+        if len(sys.argv) == 2:
+            if 'start' == sys.argv[1]:
+                daemon.start()
+            elif 'stop' == sys.argv[1]:
+                daemon.stop()
+            elif 'restart' == sys.argv[1]:
+                daemon.restart()
+            else:
+                print "Unknown command"
+                sys.exit(2)
+            sys.exit(0)
+        else:
+            print "usage: %s start|stop|restart" % sys.argv[0]
+            sys.exit(2)
+
+
 if __name__ == "__main__":
     daemon = IrcDaemon('/tmp/{}.pid'.format(sys.argv[0]))
+    daemon.main() # run it
 
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print "Unknown command"
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
